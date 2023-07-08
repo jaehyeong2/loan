49adb699d112948a6b00773121cd936ab91afa2c
jaehyeong <wogud1514@gmail.com>
2023-07-08 16:47:21 +0900

    init

---
+++ src/main/kotlin/jjfactory/loan/LoanApplication.kt
@@ -0,0 +1,11 @@
+package jjfactory.loan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoanApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoanApplication>(*args)
+}
